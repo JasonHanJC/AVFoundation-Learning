c6a8a6d73410214cf02777352b6e2d2f040f7c7f
Juncheng Han <namrie1990@gmail.com>
2017-08-14 21:29:26 -0700

    init AudioPlayer

---
+++ AudioPlayer/AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by Juncheng Han on 8/14/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioPlayer/AudioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AudioPlayer
+//
+//  Created by Juncheng Han on 8/14/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
