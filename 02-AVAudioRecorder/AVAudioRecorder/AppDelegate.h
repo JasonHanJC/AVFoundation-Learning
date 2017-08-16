3f98835246f98ec51eda70e864ec0e10d2a453e3
Juncheng Han <namrie1990@gmail.com>
2017-08-15 17:46:42 -0700

    init AVAudioPlayer test app

---
+++ 02-AVAudioRecorder/AVAudioRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVAudioRecorder
+//
+//  Created by Juncheng Han on 8/15/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02-AVAudioRecorder/AVAudioRecorder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAudioRecorder
+//
+//  Created by Juncheng Han on 8/15/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
