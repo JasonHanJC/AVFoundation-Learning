dc8fd61cb8a9a5e40a8c9f133249be0f436515b4
Juncheng Han <namrie1990@gmail.com>
2017-08-21 17:04:47 -0700

    init avplayer test app

---
+++ 03-AVPlayer/AVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVPlayer
+//
+//  Created by Juncheng Han on 8/21/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 03-AVPlayer/AVPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AVPlayer
+//
+//  Created by Juncheng Han on 8/21/17.
+//  Copyright © 2017 Jason H. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
